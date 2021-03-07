89eb513ae9d1066d5fecfd249a239e7682bbaa25
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-03-07 10:12:57 +0200

    add new class with consts

---
+++ src/main/java/testngproject/browser/BrowserNames.java
@@ -0,0 +1,10 @@
+package testngproject.browser;
+
+public class BrowserNames {
+
+    private BrowserNames() {
+    }
+
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+}
