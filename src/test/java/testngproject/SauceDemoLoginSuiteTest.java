fb231807d6e0da0b85c065abf829be657165f253
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-03-07 07:47:52 +0200

    add new test and vars

--- src/test/java/testngproject/SauceDemoLoginSuiteTest.java
+++ src/test/java/testngproject/SauceDemoLoginSuiteTest.java
@@ -1,5 +1,6 @@
 package testngproject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -8,10 +9,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import testngproject.selectors.Selectors;
 import testngproject.urls.Urls;
 
 public class SauceDemoLoginSuiteTest {
     WebDriver driver;
+    String actualErrorMessage, expectedErrorMessage;
 
     @BeforeMethod
     public void setUp() {
@@ -26,6 +29,14 @@ public class SauceDemoLoginSuiteTest {
     public void validateTheCurrentUrl() {
         String currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, Urls.SAUCER_DEMO_LOGIN_PAGE);
+    }
+
+    @Test
+    public void clickOnTheLoginButtonWithEmptyForm() {
+        expectedErrorMessage = "Epic sadface: Username is required";
+        driver.findElement(By.id(Selectors.LOGIN_BUTTON)).click();
+        actualErrorMessage = driver.findElement(By.cssSelector(Selectors.ERROR_MESSAGE)).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
     }
 
     @AfterMethod
