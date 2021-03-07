2e0fbf740f4e11ba37636fe555c9461ae02078dd
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-03-07 06:54:52 +0200

    add first test

---
+++ src/test/java/testngproject/SauceDemoLoginSuiteTest.java
@@ -0,0 +1,34 @@
+package testngproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import testngproject.urls.Urls;
+
+public class SauceDemoLoginSuiteTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(Urls.SAUCER_DEMO_LOGIN_PAGE);
+    }
+
+    @Test
+    public void validateTheCurrentUrl() {
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, Urls.SAUCER_DEMO_LOGIN_PAGE);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+}
