fc41b89471f76281c182dad46b5d2d832412c840
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-03-07 09:55:25 +0200

    update test

--- src/test/java/testngproject/SauceDemoLoginSuiteTest.java
+++ src/test/java/testngproject/SauceDemoLoginSuiteTest.java
@@ -1,25 +1,21 @@
 package testngproject;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import testngproject.browser.Browser;
 import testngproject.selectors.Selectors;
 import testngproject.urls.Urls;
 
-public class SauceDemoLoginSuiteTest {
-    WebDriver driver;
+public class SauceDemoLoginSuiteTest extends Browser {
     String actualErrorMessage, expectedErrorMessage;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        setUpBrowser("chrome");
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get(Urls.SAUCER_DEMO_LOGIN_PAGE);
